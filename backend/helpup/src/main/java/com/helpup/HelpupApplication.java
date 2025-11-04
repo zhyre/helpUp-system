a5d71341f5ccd868e4b8a6c102a413e6f0a59699
zhyre <zhyrecoritico@gmail.com>
2025-11-04 18:42:37 +0800

    added backend springboot

---
+++ backend/helpup/src/main/java/com/helpup/HelpupApplication.java
@@ -0,0 +1,13 @@
+package com.helpup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpupApplication.class, args);
+	}
+
+}
---
+++ backend/helpup/src/test/java/com/helpup/HelpupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
