0226bf2de0d5e796fd12ff12700ba40f2a2b7736
zhyre <zhyrecoritico@gmail.com>
2025-11-04 18:26:16 +0800

    added react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg'; // We removed the logo, so this isn't needed
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hi devs</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
